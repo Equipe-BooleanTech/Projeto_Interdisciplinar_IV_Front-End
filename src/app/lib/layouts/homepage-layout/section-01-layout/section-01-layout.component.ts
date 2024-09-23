dd70702e67c97a3309882eff00d889ff2840dfb1
Thais Andrade <thais.andrade11@fatec.sp.gov.br>
2024-09-23 19:21:56 -0300

    Feat: Formulário de Login

---
+++ src/app/lib/layouts/homepage-layout/section-01-layout/section-01-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-01-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './section-01-layout.component.html',
+  styles: ``
+})
+export class Section01LayoutComponent {
+
+}
