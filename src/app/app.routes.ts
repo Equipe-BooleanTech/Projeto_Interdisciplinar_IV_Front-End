ff21654a7b66f11fca2d8925791ff9352a64910b
Thais Andrade <thais.andrade11@fatec.sp.gov.br>
2024-09-30 19:26:23 -0300

    feat: Ajustes páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import {
     NotFoundComponent,
     PainelContadorComponent,
 } from '@presentation/view/pages';
+import { ColaboradorComponent } from '@presentation/view/pages/admin/dashboard/colaborador/colaborador.component';
 import { ComponentsLayoutComponent } from '@presentation/view/pages/components-layout/components-layout.component';
 
 export const routes: Routes = [
@@ -21,6 +22,10 @@ export const routes: Routes = [
     {
         path: 'admin/controle-caixa/painel-contador',
         component: PainelContadorComponent,
+    },
+    {
+        path: 'admin/colaborador',
+        component: ColaboradorComponent,
     },
     { path: 'teste', component: ComponentsLayoutComponent },
     { path: '**', component: NotFoundComponent },
