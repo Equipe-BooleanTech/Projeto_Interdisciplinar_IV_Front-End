2e67e8a56370a1525e6fce74544c0101744034e0
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-10-07 21:21:36 -0300

    bugfix: sidebar and pie chart

--- src/app/presentation/view/pages/admin/stock-control/stock-control.component.ts
+++ src/app/presentation/view/pages/admin/stock-control/stock-control.component.ts
@@ -1,13 +1,98 @@
-import { Component } from '@angular/core';
-import { ButtonComponent, CardComponent, SidebarComponent } from '@presentation/view/components';
+import { Component, AfterViewInit } from '@angular/core';
+import { PieChartOptions, PieMetrics } from '@domain/interfaces';
+import {
+    ButtonComponent,
+    CardComponent,
+    SidebarComponent,
+    PieComponent,
+} from '@presentation/view/components';
+
+import ApexCharts from 'apexcharts';
 
 @Component({
-  selector: 'app-stock-control',
-  standalone: true,
-  imports: [SidebarComponent, CardComponent, ButtonComponent],
-  templateUrl: './stock-control.component.html',
-  styles: ``
+    selector: 'app-stock-control',
+    standalone: true,
+    imports: [SidebarComponent, CardComponent, ButtonComponent, PieComponent],
+    templateUrl: './stock-control.component.html',
+    styles: ``,
 })
-export class StockControlComponent {
+export class StockControlComponent implements AfterViewInit {
+    constructor() {}
 
+    metrics: PieMetrics = {
+        title: 'Proporções de estoque',
+        dateRange: '08/10/2023 - 08/10/2024',
+        options: [
+            { href: '#', text: 'Exportar' },
+            { href: '#', text: 'Compartilhar' },
+        ],
+    };
+    data: PieChartOptions = {
+        type: 'pie',
+        series: [12.8, 26.8, 20.4, 40],
+        colors: ['#740318', '#FDBA8C', '#118632', '#F4BE3775'],
+        chart: {
+            height: '100%',
+            width: '100%',
+            type: 'pie',
+        },
+        stroke: {
+            colors: ['white'],
+            lineCap: '',
+        },
+        plotOptions: {
+            pie: {
+                labels: {
+                    show: true,
+                },
+                size: '100%',
+                dataLabels: {
+                    offset: -25,
+                },
+            },
+        },
+        labels: ['Ingredientes', 'Fornecedores', 'Fichas Técnicas', 'Pratos'],
+        dataLabels: {
+            enabled: true,
+            style: {
+                fontFamily: 'DM Sans, sans-serif',
+            },
+        },
+        legend: {
+            position: 'bottom',
+            fontFamily: 'DM Sans, sans-serif',
+        },
+        yaxis: {
+            labels: {
+                formatter: function (value) {
+                    return value + '%';
+                },
+            },
+        },
+        xaxis: {
+            labels: {
+                formatter: function (value) {
+                    return value + '%';
+                },
+            },
+            axisTicks: {
+                show: false,
+            },
+            axisBorder: {
+                show: false,
+            },
+        },
+    };
+    ngAfterViewInit(): void {
+        if (
+            document.getElementById('stock-chart') &&
+            typeof ApexCharts !== 'undefined'
+        ) {
+            const chart = new ApexCharts(
+                document.getElementById('stock-chart'),
+                this.data,
+            );
+            chart.render();
+        }
+    }
 }
