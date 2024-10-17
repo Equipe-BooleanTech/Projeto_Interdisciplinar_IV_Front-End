c5a391272555d272d855e91d5cf2e08a526a4e7d
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-10-17 20:57:23 -0300

    feat: card dinamico

--- src/app/presentation/view/pages/shared/test/test.component.ts
+++ src/app/presentation/view/pages/shared/test/test.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { CardComponent } from "../../../components/card/card.component";
-import { SidebarComponent } from "../../../components/sidebar/sidebar.component";
+import { CardComponent } from '../../../components/card/card.component';
+import { SidebarComponent } from '../../../components/sidebar/sidebar.component';
 
 @Component({
-  selector: 'app-test',
-  standalone: true,
-  imports: [CardComponent, SidebarComponent],
-  templateUrl: './test.component.html',
-  styles: ``
+    selector: 'app-test',
+    standalone: true,
+    imports: [CardComponent, SidebarComponent],
+    templateUrl: './test.component.html',
+    styles: ``,
 })
-export class TestComponent {
-
-}
+export class TestComponent {}
